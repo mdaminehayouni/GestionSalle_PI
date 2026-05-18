54ea8f73541b8b6138ecafaa4ae348d10873c486
hayouniamine <aammiinneehayouni@gmail.com>
2026-05-18 19:23:57 +0100

    added presentation

--- bd/gestionSalles2.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO seance (matiere, date, heure_deb, heure_fin, enseignantId, chefDepartement, classeId, salleId) VALUES
-('Base de données', '2026-04-21', '08:30:00', '11:30:00', 1, NULL, 1, 1),
-('Algorithmique', '2026-04-21', '11:30:00', '13:30:00', 2, NULL, 1, 2),
-('Web Development', '2026-04-22', '14:00:00', '17:00:00', 3, NULL, 2, 4),
-('POO Java', '2026-04-22', '08:30:00', '10:30:00', 1, NULL, 2, 5),
-('Réseaux', '2026-04-23', '10:30:00', '12:30:00', 2, NULL, 3, 1),
-('Math Discrete', '2026-04-23', '14:30:00', '17:30:00', 4, NULL, 4, 3),
-('Analyse', '2026-04-24', '08:30:00', '12:30:00', 4, NULL, 5, 11),
-('Systemes', '2026-04-25', '09:00:00', '12:00:00', 3, NULL, 3, 12);
